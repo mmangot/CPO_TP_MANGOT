7779aae3f6969806f42a2b728db6ff0cf683f651
Manon MANGOT <manon.mangot@epfedu.fr>
2023-09-18 15:18:12 +0200

    Création d'un nouveau projet versionné

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,27 @@
+/* MANGOT Manon
+*TP0
+*TDA2
+*18 septembre 2023
+*/
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author manon
+ */
+public class Exo1 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("au revoir");
+        String prenom;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, comme t'appelles-tu ?");
+prenom = sc.nextLine();
+    }
+    
+}

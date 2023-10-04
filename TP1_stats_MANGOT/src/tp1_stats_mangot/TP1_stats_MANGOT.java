710331077ed1fae70fce54ee68f59176e3250d8f
Manon MANGOT <manon.mangot@epfedu.fr>
2023-10-04 10:58:46 +0200

    partie 3 operationnelle sans le bonus

---
+++ TP1_stats_MANGOT/src/tp1_stats_mangot/TP1_stats_MANGOT.java
@@ -0,0 +1,21 @@
+/* MANGOT Manon
+*TP1
+*TDA2
+*4 octobre 2023 2023
+ */
+package tp1_stats_mangot;
+
+/**
+ *
+ * @author manon
+ */
+public class TP1_stats_MANGOT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

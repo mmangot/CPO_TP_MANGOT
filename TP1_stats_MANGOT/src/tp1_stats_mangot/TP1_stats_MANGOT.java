182c5ff065c446ab611defbfcece76faf13e68c3
Manon MANGOT <manon.mangot@epfedu.fr>
2023-10-05 19:02:22 +0200

    partie 4 terminée

--- TP1_stats_MANGOT/src/tp1_stats_mangot/TP1_stats_MANGOT.java
+++ TP1_stats_MANGOT/src/tp1_stats_mangot/TP1_stats_MANGOT.java
@@ -5,6 +5,9 @@
  */
 package tp1_stats_mangot;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author manon
@@ -15,15 +18,25 @@ public class TP1_stats_MANGOT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         int[] tableau = new int[6];
-        
-        for (int i = 0; i < tableau.length; i++) {
-            tableau[i] = 0;
+         int[] des = new int[6];
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Veuillez entrer un nombre entier m : ");
+        int m = scanner.nextInt();
+        Random random = new Random();
+        for (int i = 0; i < m; i++) {
+            int resultat = random.nextInt(6);
+            des[resultat]++;
         }
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        System.out.println("saisir un entier");
-        int m = sc.nextInt();
-        // TODO code application logic here
+
+        System.out.println("Résultat du lancer de dés :");
+        for (int i = 0; i < des.length; i++) {
+            double pourcentage = ((double) des[i] / m) * 100;
+            System.out.println("Face " + (i + 1) + " : " + pourcentage +"%" );
+        }
     }
-    
 }
+        // TODO code application logic here
+    
+    
+

4f86ce2bcd095ab73a7b0aadc42225c1d716d9a2
Manon MANGOT <manon.mangot@epfedu.fr>
2023-10-06 15:22:33 +0200

    TP2 non fini

--- TP2_Bieres_MANGOT/src/tp2_bieres_mangot/TP2_Bieres_MANGOT.java
+++ TP2_Bieres_MANGOT/src/tp2_bieres_mangot/TP2_Bieres_MANGOT.java
@@ -15,25 +15,26 @@ public class TP2_Bieres_MANGOT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BouteilleBiere uneBiere = new BouteilleBiere() ;
-        uneBiere.nom = "Cuvée des trolls";
-        uneBiere.degreAlcool = 7.0 ;
-        uneBiere.brasserie = "Dubuisson";
-        uneBiere.ouverte=false;
-        
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson",false);
         uneBiere.lireEtiquette();
+        uneBiere.Decapsuler();
 
-        BouteilleBiere deuxiemeBiere = new BouteilleBiere();
-        deuxiemeBiere.nom = "Leffe";
-        deuxiemeBiere.degreAlcool = 6.6 ;
-        deuxiemeBiere.brasserie = "Abbaye de Leffe";
-        
+        BouteilleBiere deuxiemeBiere = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe",true);
         deuxiemeBiere.lireEtiquette();
+        deuxiemeBiere.Decapsuler();
         
+        BouteilleBiere troisiemeBiere = new BouteilleBiere("Monartistes", 10.6, "Les petits dragons",false);
+        troisiemeBiere.lireEtiquette();
+        troisiemeBiere.Decapsuler();
         
-
-
+        BouteilleBiere quatriemeBiere = new BouteilleBiere("Copacabaners", 12.6, "La victoire",false);
+        quatriemeBiere.lireEtiquette();
+        quatriemeBiere.Decapsuler();
+        
+        BouteilleBiere cinquiemeBiere = new BouteilleBiere("Cow'Brazz", 9.6, "Tout l'epf s'embrasse",true);
+        cinquiemeBiere.lireEtiquette();
+        cinquiemeBiere.Decapsuler();
         // TODO code application logic here
     }
-    
+
 }

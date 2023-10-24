27155a156742b0c7672cbe905d41cbefa8c6a96a
Manon MANGOT <manon.mangot@epfedu.fr>
2023-10-24 22:26:19 +0200

    TP3 question correction bug

--- TP3_Heroic_Fantasy_MANGOT/src/Personnages/Magicien.java
+++ TP3_Heroic_Fantasy_MANGOT/src/Personnages/Magicien.java
@@ -5,29 +5,66 @@
 package Personnages;
 
 import Personnages.Personnage;
+import Armes.Arme;
+import Armes.Baton;
+import java.util.ArrayList;
 
 /**
  *
  * @author manon
  */
 public class Magicien extends Personnage{
-    private boolean confirme ;  
-
-    public void setConfirme(boolean confirme) {
-        this.confirme = confirme;
-    }
-
-    public boolean isConfirme() {
-        return confirme;
-    }
+    boolean confirme;
+    String verif;
+    ArrayList <Arme> tabArmes= new ArrayList<Arme>();
+    Arme armeMain;
+    int nbMag;
     
-    
-    public Magicien(String Nom, int NiveauVie, String C) {
+    public Magicien(String Nom, int NiveauVie, boolean conf) {
         super(Nom, NiveauVie);
-        if ("confirme".equals(C))
-            confirme =true;
-        else confirme = false;
+        confirme=conf;
+        armeMain=null;
+        nbMag+=1;
+        
+        if (confirme==true){
+            verif="confirme";
+        }
+        else{
+            verif="novice";
+        }
     }
+    
+    public boolean ajouterArme (Arme armeAjoute){
+        if (tabArmes.size()==5){
+            System.out.println("trop d'armes");
+            return false;
+        }
+        else{
+            tabArmes.add(armeAjoute);
+            return true;
+        }
+    }
+    public void ChoixArme (Arme arme){
+        if (tabArmes.contains(arme)){
+            armeMain=arme;
+        }
+        else{
+            System.out.println("arme non dispo");
+        }
+      
+    }
+    public Arme getarmeMain(){
+        return armeMain;
+    }
+    
+    public String equipeArme (Arme arme){
+        if (tabArmes.contains(arme)){
+            armeMain=arme;
+            return nom+"equipe avec "+armeMain;
+    }
+        else{
+            return nom+" a pas l'arme";
+        }
     public String toString() {
         return "Nom de l'arme : " + nom + ", Niveau vie : " + niveauVie+" Confirme ? "+confirme;
     

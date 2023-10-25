df0fdddabf450656fbb54f13dbb2e5a6684921d4
Manon MANGOT <manon.mangot@epfedu.fr>
2023-10-25 15:22:15 +0200

    Projet Lightoff, Classe GrilleDeJeu ok sauf toString + debut Classe Partie

---
+++ LightOff_MANGOT_version_console/src/lightoff_mangot_version_console/Partie.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_mangot_version_console;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author manon
+ */
+public class Partie {
+    GrilleDeJeu grille;
+    int nbCoups;
+    
+    public Partie() {
+        grille = new GrilleDeJeu(); 
+        nbCoups = 0;}
+    
+    public void initialiserPartie() {
+        grille.melangerMatriceAleatoirement(4);}
+    
+     public void lancerPartie() {
+         initialiserPartie(); 
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (!grille.cellulesToutesEteintes()) {
+            afficherEtatGrille();
+            demanderCoupAuJoueur(scanner);
+            nbCoups++;
+        }
+
+        afficherEtatGrille();
+        afficherResultats();
+    }
+
+   
+    private void afficherEtatGrille() {
+        System.out.println(grille);
+    }
+
+    
+    private void demanderCoupAuJoueur(Scanner scanner) {
+        System.out.print("Entrez les coordonnées (ligne, colonne) de la cellule à activer : ");
+        int ligne = scanner.nextInt();
+        int colonne = scanner.nextInt();
+
+        
+        grille.activerCellule();
+    }
+
+    
+    private void afficherResultats() {
+        System.out.println("Toutes les cellules sont éteintes en " + nbCoups + " coups !");
+    }
+}
